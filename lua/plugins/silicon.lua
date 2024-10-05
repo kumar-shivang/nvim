27aabfd03fb1f6d58cd93c733131af6ca1722ef3
kumar-shivang <shivangkumar1857@gmail.com>
2024-10-05 18:17:45 +0530

    chore: Update Neovim configuration with lazy loading for plugins and key mappings in silicon.lua

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -1,30 +1,19 @@
 -- Adding silicon.nvim plugin
 
 return {
-  "michaelrommel/nvim-silicon",
-  lazy = true,
-  cmd = "Silicon",
-  init = function()
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>S"] = { ":Silicon<CR>", "Silicon: Capture code to image" },
-    }, { mode = "v" })
-    wk.register({
-      ["<leader>S"] = { ":Silicon<CR>", "Silicon: Capture code to image" },
-      { mode = "n" },
-    })
-  end,
-  config = function()
-    require("silicon").setup({
-      font = "FiraCode Nerd Font=34; Noto Color Emoji=34",
-      theme = "DarkNeon",
-      output = function()
-        os.execute("mkdir -p ~/Pictures/Silicon")
-        return "~/Pictures/Silicon/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
-      end,
-      window_title = function()
-        return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
-      end,
-    })
-  end,
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	main = "nvim-silicon",
+	opts = {
+		font = "FiraCode Nerd Font=34; Noto Color Emoji=34",
+		theme = "DarkNeon",
+		output = function()
+			os.execute("mkdir -p ~/Pictures/Silicon")
+			return "~/Pictures/Silicon/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+		end,
+		window_title = function()
+			return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+		end,
+	},
 }
