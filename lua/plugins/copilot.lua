dc1a5f5e7ae0719914c9546859febf2fb567e325
kumar-shivang <shivangkumar1857@gmail.com>
2024-12-11 18:41:36 +0530

    refactor: Remove powermode plugin and update configuration files

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,32 +1,32 @@
-return {
-  { "zbirenbaum/copilot-cmp", enabled = false },
-  {
-    "zbirenbaum/copilot.lua",
-    enabled = true,
-    config = function()
-      require("copilot").setup({
-        panel = {
-          enabled = false,
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          debounce = 75,
-          keymap = {
-            accept = "<M-'>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        filetypes = {
-          ["."] = true,
-        },
-        copilot_node_command = "node", -- Node.js version must be > 18.x
-        server_opts_overrides = {},
-      })
-    end,
-  },
-}
+return {}
+--   { "zbirenbaum/copilot-cmp", enabled = false },
+--   {
+--     "zbirenbaum/copilot.lua",
+--     enabled = true,
+--     config = function()
+--       require("copilot").setup({
+--         panel = {
+--           enabled = false,
+--         },
+--         suggestion = {
+--           enabled = true,
+--           auto_trigger = true,
+--           debounce = 75,
+--           keymap = {
+--             accept = "<M-'>",
+--             accept_word = false
+--             accept_line = false,
+--             next = "<M-]>",
+--             prev = "<M-[>",
+--             dismiss = "<C-]>",
+--           },
+--         },
+--         filetypes = {
+--           ["."] = true,
+--         },
+--         copilot_node_command = "node", -- Node.js version must be > 18.x
+--         server_opts_overrides = {},
+--       })
+--     end,
+--   },
+-- }
